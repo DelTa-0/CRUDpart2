d279e703b9c237eaa9e7877fba20295bf99ecb25
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-03 14:02:27 +0545

    update functionality added

--- index.js
+++ index.js
@@ -28,8 +28,18 @@ app.post('/create',async(req,res)=>{
 })
 
 app.get('/delete/:id',async(req,res)=>{
-    let deleteduser=await userModel.findOneAndDelete({_id:req.params.id});
+    let user=await userModel.findOneAndDelete({_id:req.params.id});
     res.redirect('/read');
 })
 
+app.get('/edit/:id',async(req,res)=>{
+    let user=await userModel.findOne({_id:req.params.id});
+    res.render('edit',{user});
+})
+
+app.post('/update/:userid',async(req,res)=>{
+    let {name,email,imageurl}=req.body;
+    let user=await userModel.findOneAndUpdate({_id:req.params.userid},{name,email,imageurl},{new:true});
+    res.redirect('/read');
+})
 app.listen(3000);
