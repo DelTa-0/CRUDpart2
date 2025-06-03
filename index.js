b3e1dc10bf522946d6728df046892240e19907da
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-03 13:45:52 +0545

    delete added

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ app.post('/create',async(req,res)=>{
         email,
         imageurl
     })
-    res.send(createduser)
+    res.redirect('read')
+})
+
+app.get('/delete/:id',async(req,res)=>{
+    let deleteduser=await userModel.findOneAndDelete({_id:req.params.id});
+    res.redirect('/read');
 })
 
 app.listen(3000);
